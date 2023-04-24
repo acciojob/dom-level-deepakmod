b5a069e9b924fb832d1130f64a04a7eb19f078c6
Deepak Modanwal <118596712+deepakmod@users.noreply.github.com>
2023-04-24 13:39:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+/*  your css code here. If applicable */
+let level=document.getElementById("level");
+let ul=document.getElementsByTagName("ul")[0];
+for(let i=0;i<ul.children.length;i++){
+    if(ul.children[i]==level)
+        alert("The level of the element is: "+(i+1));
+}
